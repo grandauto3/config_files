202b24c8ba54be8fd3a73db5a533316fb4d5165e
grandauto3 <39314276+grandauto3@users.noreply.github.com>
2025-08-15 08:50:47 +0200

    edit install.sh for gnu stow usage

--- install.sh
+++ install.sh
@@ -2,13 +2,3 @@
 
 # install all necessary packages
 sudo pacman -S neovim alacritty zellij fish gitui ttf-jetbrains-mono-nerd
-
-mkdir ~/.config/alacritty
-cp -r ./alacritty ~/.config/
-
-mkdir ~/.config/fish
-cp -r ./fish ~/.config/
-
-mkdir ~/.config/zellij
-cp -r ./zellij ~/.config/
-
